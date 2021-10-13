62dab21cdaa5b7758e4abc7e8465c96ac1fb8ea3
hyeongwoo94 <87638160+hyeongwoo94@users.noreply.github.com>
2021-10-13 17:52:27 +0900

    10-13

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+function Food({yaho}){
+  
+  return(
+    <h2>나는 {yaho}</h2>
+  )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>나는 형우다.</h1>
+      <Food yaho="apple" />
+      <Food yaho="라면" />
+      <Food yaho="고기" />
+      <Food yaho="밥" />
     </div>
   );
 }
